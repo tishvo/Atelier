be8c7e599c576cc1657c8e0cbe1a328e075364db
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-04-14 15:47:58 -0400

    appended RelatedItemsAndComparison to the main app dom

--- client/app.jsx
+++ client/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import QandA_app from './components/Q&A/QandA_app.jsx';
+import RelatedItemsAndComparison from './components/RelatedItemsAndComparison/index.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class App extends React.Component {
     return (
       <div>
         <div>HELLO</div>
+        <RelatedItemsAndComparison />
         <QandA_app />
       </div>
     )
---
+++ client/components/RelatedItemsAndComparison/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+class RelatedItemsAndComparison extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Related Items And Comparison View</div>
+      </div>
+    )
+  }
+}
+
+export default RelatedItemsAndComparison;
