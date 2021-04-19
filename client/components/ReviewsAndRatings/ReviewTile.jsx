eb1ed8cf4fdc005e2dd3753be42729b9059f696a
Mark McLindon <clindo89@gmail.com>
2021-04-19 18:33:25 -0400

    added name and date div

--- client/components/ReviewsAndRatings/ReviewTile.jsx
+++ client/components/ReviewsAndRatings/ReviewTile.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import StarRating from './StarRating.jsx';
+import moment from 'moment';
 
 const ReviewTile = (props) => {
+  console.log('Review Tile reviewData: ', props.reviewData);
   return (
     <div>
-      <StarRating />
+      <StarRating stars={props.reviewData.rating}/>
+      <div>{props.reviewData.reviewer_name}, {moment(props.reviewData.date).format('MMM Do YYYY')}</div>
+      <p>Review Summary:{props.reviewData.summary}</p>
       <p>Review Body:{props.reviewData.body}</p>
     </div>
   )
