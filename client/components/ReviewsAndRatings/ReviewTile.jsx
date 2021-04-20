67f024b9160bf1ee0b8970f42898188b4505bd5e
Mark McLindon <clindo89@gmail.com>
2021-04-20 12:56:10 -0400

    made state component to handle showing responses

--- client/components/ReviewsAndRatings/ReviewTile.jsx
+++ client/components/ReviewsAndRatings/ReviewTile.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import StarRating from './StarRating.jsx';
 import moment from 'moment';
 
-const ReviewTile = (props) => {
-  //console.log('Review Tile reviewData: ', props.reviewData);
-  return (
-    <div>
-      <StarRating stars={props.reviewData.rating}/>
-      <div>{props.reviewData.reviewer_name}, {moment(props.reviewData.date).format('MMM Do YYYY')}</div>
-      <p>Review Summary:{props.reviewData.summary}</p>
-      <p>Review Body:{props.reviewData.body}</p>
-    </div>
-  )
+class ReviewTile extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <StarRating stars={this.props.reviewData.rating}/>
+        <div>{this.props.reviewData.reviewer_name}, {moment(this.props.reviewData.date).format('MMM Do YYYY')}</div>
+        <p>Review Summary:{this.props.reviewData.summary}</p>
+        <p>Review Body:{this.props.reviewData.body}</p>
+        {/*How do we want to handle a response? make a whole component?
+          i could handle it with state
+        */}
+      </div>
+    )
+  }
 }
 
 export default ReviewTile;
