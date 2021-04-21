b9aca1918152e98a6353195c67c754af86ce0391
Mark McLindon <clindo89@gmail.com>
2021-04-21 15:23:29 -0400

    Now shows images if they exist

--- client/components/ReviewsAndRatings/ReviewTile.jsx
+++ client/components/ReviewsAndRatings/ReviewTile.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import StarRating from './StarRating.jsx';
 import moment from 'moment';
-import {FaCheck} from 'react-icons/fa'
+import { FaCheck } from 'react-icons/fa'
 
-class ReviewTile extends React.Component{
+class ReviewTile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       responseVisible: true,
       helpfulYes: this.props.reviewData.helpfulness,
       helpfulHasBeenClicked: false,
-      recommend: false
+      recommend: false,
+      arrayOfPhotos: []
     };
     this.helpfulClick = this.helpfulClick.bind(this);
   }
 
   componentDidMount() {
     //check if there is a response to a review, set responseVisible to true
-    if(this.props.reviewData.response === null) {
+    if (this.props.reviewData.response === null) {
       this.setState({
         responseVisible: false
       });
@@ -28,14 +29,20 @@ class ReviewTile extends React.Component{
     }
 
     //check if the current product is recommended, set to true if recommended
-    if(this.props.reviewData.recommend) {
+    if (this.props.reviewData.recommend) {
       //console.log('is this product recommended: ', this.props.reviewData.recommend);
       this.setState({
         recommend: true
       });
     };
 
-  };
+    //check to see if the current review contains images
+    if (this.props.reviewData.photos.length > 0) {
+      this.setState({
+        arrayOfPhotos: this.props.reviewData.photos
+      });
+    }
+  }
 
   //only allows for one yes click, adds one to helpful
   helpfulClick(event) {
@@ -48,16 +55,26 @@ class ReviewTile extends React.Component{
     }
   }
 
+
+
   render() {
-    //console.log('is state changing? ', this.state.responseVisible);
-    //console.log('reviewData: ', this.props.reviewData);
+
     return (
       <div>
-        <StarRating stars={this.props.reviewData.rating}/>
+        <StarRating stars={this.props.reviewData.rating} />
         <div>{this.props.reviewData.reviewer_name}, {moment(this.props.reviewData.date).format('MMM Do YYYY')}</div>
+        {this.state.arrayOfPhotos.length > 0 ?
+
+          this.state.arrayOfPhotos.map((photos, items) => {
+            return (
+                <img src={photos.url}></img>
+            )
+          })
+
+          : null}
         <div>Review Summary: {this.props.reviewData.summary}</div>
         <div>Review Body: {this.props.reviewData.body}</div>
-        {this.state.recommend ? <p><FaCheck/> I recommend this product</p> : null}
+        {this.state.recommend ? <p><FaCheck /> I recommend this product</p> : null}
         {this.state.responseVisible ? <p>Response: {this.props.reviewData.response}</p> : null}
         <div>Helpful?
           <div onClick={this.helpfulClick}>Yes</div>
