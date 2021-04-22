a94f06870e1c91c5f1756755f153ad4eab35ac5a
rriegel <ryan.j.riegel@gmail.com>
2021-04-21 20:50:57 -0400

    adjusted how I declared React useState and useEffect - in the import statement now

--- client/components/RelatedItemsAndComparison/subcomponents/YourOutfit.jsx
+++ client/components/RelatedItemsAndComparison/subcomponents/YourOutfit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Arrow from './Arrow.jsx';
 import YOCard from './YOCard.jsx';
@@ -35,7 +35,7 @@ const YourOutfit = (props) => {
     });
   }
 
-  React.useEffect(() => {
+  useEffect(() => {
     updateState();
   }, [console.log('state after useEffect: ', state)])
 
