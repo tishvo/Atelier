65e56358a5985dea1bfe36c6c86eef5802d1a4c4
Tish Vo <tishvo@gmail.com>
2021-04-22 20:08:52 -0400

    added additional jest tests

--- client/components/Q&A/QandA_app.jsx
+++ client/components/Q&A/QandA_app.jsx
@@ -82,12 +82,12 @@ class QandA_app extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="questionsandanswers">
         <h1><div>Questions <span>&amp;</span> Answers</div></h1>
-        <div><QA_search onSearch={this.onSearch}/></div>
-        <div><QA_list qa={this.state.defaultq4} selected={this.state.selected}/></div>
+        <div className="qasearch"><QA_search onSearch={this.onSearch}/></div>
+        <div className="qalist"><QA_list qa={this.state.defaultq4} selected={this.state.selected}/></div>
         <div><button id="maq" onClick={this.maqClick}>More Answered Questions</button><button onClick={e => { this.showModal(); }} className="qaModalToggle">Add A Question +</button></div>
-        <div><AddQModal show={this.state.addQ} product={this.state.selected} onClose={this.showModal}/></div>
+        <div className="addqmodal"><AddQModal show={this.state.addQ} product={this.state.selected} onClose={this.showModal}/></div>
       </div>
     )
   }
